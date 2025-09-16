72cdc9b9fd330b0a64d4b9be781455fa7045eab1
DS4506 <davidsmith4506@gmail.com>
2025-09-15 20:49:17 -0500

    Initial Commit

---
+++ Hobbies/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hobbies
+//
+//  Created by Willie Earl on 9/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
